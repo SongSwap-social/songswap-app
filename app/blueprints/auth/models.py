09d3f2f083a1879086e3be5e1101a6278b8c03ab
Sason <sasonreza@gmail.com>
2023-05-20 22:55:20 -0700

    Utilize Authlib, allow login with Spotify

--- app/blueprints/auth/models.py
+++ app/blueprints/auth/models.py
@@ -1,16 +1,13 @@
+import flask_login
+
 from app.database import db
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class User(db.Model, flask_login.UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     spotify_id = db.Column(db.String(120), unique=True, nullable=False)
-    # Add other necessary columns
-    access_token = db.Column(db.String(120), nullable=False)
-    refresh_token = db.Column(db.String(120), nullable=False)
-    # example `expires_at` value: 1679647406
-    expires_at = db.Column(db.Integer, nullable=False)
     # Define relationships with other models (e.g., posts, groups)
 
     # Flask-Login integration
