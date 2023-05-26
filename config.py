8c573ff2bb120f9b694a6b8d84b543feaa62dd96
Sason <sasonreza@gmail.com>
2023-05-25 21:33:30 -0700

    Create Dockerfile for deploying songswap-app

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
+DEBUG = environ.get("DEBUG")
 SECRET_KEY = environ.get("SECRET_KEY")
 SPOTIFY_CLIENT_ID = environ.get("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = environ.get("SPOTIFY_CLIENT_SECRET")
