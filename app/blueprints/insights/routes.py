38f9b725464be1b4b15e4ef811fe9269502eb1e1
Sason <sasonreza@gmail.com>
2023-06-25 21:19:42 -0700

    Create initial global insights page

--- app/blueprints/insights/routes.py
+++ app/blueprints/insights/routes.py
@@ -1,25 +1,25 @@
 # app/blueprints/insights/routes.py
-from flask import Blueprint, abort, render_template
-from flask_login import current_user
 import requests
+from flask import Blueprint, abort, render_template
+from flask_login import current_user, login_required
 
 insights_bp = Blueprint("insights", __name__)
 
+TOP_LIMIT = 10
+
 
 @insights_bp.route("/insights")
+@login_required
 def home():
     """Retrieve the user's top tracks from the songswap-insights API"""
 
-    # If the user is not authenticated, raise an unauthorized error
-    if not current_user.is_authenticated:
-        abort(401)
-
     # Get the user's ID from the session
-    user_id = current_user.spotify_id
+    user_id = current_user.id
 
     # Send a GET request to the songswap-insights API with the user's ID as an argument
     response = requests.get(
-        f"http://127.0.0.1:5001/insights/top-tracks", params={"user_id": user_id}
+        f"http://127.0.0.1:5001/insights/top-tracks",
+        params={"user_id": user_id, "limit": TOP_LIMIT},
     )
 
     # If the response is not successful, raise an error
@@ -34,69 +34,51 @@ def home():
     # Render the template with the data
     # return render_template("insights.html", data=data)
     # Return the data as JSON
-    return data
+    return render_template("insights.html", tracks=data)
 
 
-@insights_bp.route("/insights_mock")
-def home_mock():
-    import random
-    import datetime
+@insights_bp.route("/insights_global")
+@login_required
+def home_global():
+    """Retrieve total and distinct statistics from songswap-insights API"""
 
-    import pyecharts.options as opts
-    from pyecharts.charts import Calendar
+    total_tracks = requests.get(f"http://127.0.0.1:5001/insights/global/total/listens")
+    distinct_tracks = requests.get(
+        f"http://127.0.0.1:5001/insights/global/distinct/tracks"
+    )
+    distinct_artists = requests.get(
+        f"http://127.0.0.1:5001/insights/global/distinct/primary-artists"
+    )
+    total_listen_time = requests.get(
+        f"http://127.0.0.1:5001/insights/global/total/listen-time",
+        params={"as_string": True},
+    )
 
-    begin = datetime.date(2017, 1, 1)
-    end = datetime.date(2017, 12, 31)
-    data = [
-        [str(begin + datetime.timedelta(days=i)), random.randint(1000, 25000)]
-        for i in range((end - begin).days + 1)
+    top_tracks = requests.get(
+        "http://127.0.0.1:5001/insights/global/top/tracks", params={"limit": TOP_LIMIT}
+    )
+    top_tracks_json = top_tracks.json()
+    track_names = [track["track_name"] for track in top_tracks_json]
+    artist_names = [track["artist_name"] for track in top_tracks_json]
+    track_counts = [track["count"] for track in top_tracks_json]
+    top_tracks = [
+        {"track_name": track_name, "artist_name": artist_name, "count": count}
+        for track_name, artist_name, count in zip(
+            track_names, artist_names, track_counts
+        )
+    ]
+    top_artists = [
+        {"artist_name": artist_name, "count": count}
+        for artist_name, count in zip(artist_names, track_counts)
     ]
 
-    chart = (
-        Calendar()
-        .add(
-            series_name="",
-            yaxis_data=data,
-            calendar_opts=opts.CalendarOpts(
-                pos_top="120",
-                pos_left="30",
-                pos_right="30",
-                range_="2017",
-                yearlabel_opts=opts.CalendarYearLabelOpts(is_show=False),
-            ),
-        )
-        .set_global_opts(
-            title_opts=opts.TitleOpts(
-                pos_top="30", pos_left="center", title="2017年步数情况"
-            ),
-            visualmap_opts=opts.VisualMapOpts(
-                max_=25000, min_=500, orient="horizontal", is_piecewise=False
-            ),
-        )
-        .dump_options()
+    # Send a GET request to the songswap-insights API with the user's ID as an argument
+    return render_template(
+        "insights_global.html",
+        total_tracks=total_tracks.json(),
+        distinct_tracks=distinct_tracks.json(),
+        distinct_artists=distinct_artists.json(),
+        total_listen_time=total_listen_time.content.decode("utf-8").replace('"', ""),
+        top_tracks=top_tracks,
+        top_artists=top_artists,
     )
-
-    import random
-
-    from pyecharts import options as opts
-    from pyecharts.charts import HeatMap
-    from pyecharts.faker import Faker
-
-    value = [[i, j, random.randint(0, 80)] for i in range(24) for j in range(7)]
-    c = (
-        HeatMap()
-        .add_xaxis(Faker.clock)
-        .add_yaxis(
-            "series0",
-            [w.strip("day") for w in Faker.week_en],
-            value,
-            label_opts=opts.LabelOpts(is_show=True, position="inside"),
-        )
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="HeatMap-Label"),
-            visualmap_opts=opts.VisualMapOpts(),
-        )
-        .dump_options()
-    )
-
-    return render_template("insights.html", chart=chart)
